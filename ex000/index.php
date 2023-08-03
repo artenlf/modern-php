8ae413cf80b35474c67e6b4a27d4228c780ef068
arten <felipe.arten@gmail.com>
2023-08-03 07:02:00 -0300

    php hello, world

---
+++ ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Getting rid of the curse</title>
+</head>
+
+<body>
+  <h1>
+    <?php
+    echo "Hello, World! \u{1F30E}";
+    ?>
+  </h1>
+  <p>Let's break the curse!</p>
+</body>
+
+</html>
---
+++ ex001/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Server Data</title>
+</head>
+
+<body>
+  <h1>Server Data</h1>
+  <?php
+  phpinfo();
+  ?>
+</body>
+
+</html>
